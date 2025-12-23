c45df1a00318a52c676c25ac13a2bebd9466d42a
Harshit Pateriya <hppateriya9165@gmail.com>
2025-12-23 15:05:40 +0530

    Initial Commit

---
+++ BootIOCProj14-MiniProject-Profiles-One-YML/src/main/java/com/BootIocProj14_Working_With_Profiles_YML.java
@@ -0,0 +1,79 @@
+package com;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.controller.EmployeeOperationsController;
+import com.model.Employee;
+
+@SpringBootApplication
+public class BootIocProj14_Working_With_Profiles_YML {
+
+	public static void main(String[] args) {
+
+		System.out.println("BootIocProj05MiniProjectRealtimeDiApplication.main()-start");
+
+		try (ConfigurableApplicationContext ctx = SpringApplication
+				.run(BootIocProj14_Working_With_Profiles_YML.class, args); Scanner sc = new Scanner(System.in);) {
+
+			EmployeeOperationsController controller = ctx.getBean("empController", EmployeeOperationsController.class);
+			boolean loopVar = true;
+			while (loopVar) {
+				System.out.println(
+						"Welcome to Employee Management System :: \n1.View Records Based on Desg\n2.Insert Record\n3.Exit\nEnter Your Choice : ");
+
+				int choice = Integer.parseInt(sc.nextLine());
+
+				switch (choice) {
+				case 1 -> {
+					System.out.print("Enter Desg 1 : ");
+					String desg1 = sc.nextLine();
+					System.out.print("Enter Desg 2 : ");
+					String desg2 = sc.nextLine();
+					System.out.print("Enter Desg 3 : ");
+					String desg3 = sc.nextLine();
+
+					try {
+						List<Employee> list = controller.fetchEmployeesByDesgs(desg1, desg2, desg3);
+						list.forEach(System.out::println);
+						System.out.println();
+					} catch (Exception e) {
+						System.out.println("Internal Error----" + e.getMessage());
+						e.printStackTrace();
+					}
+				}
+
+				case 2 -> {
+					System.out.print("Enter Employee Name : ");
+					String name = sc.nextLine();
+					System.out.print("Enter Employee Desg : ");
+					String desg = sc.nextLine();
+					System.out.print("Enter Employee Salary : ");
+					Double sal = Double.parseDouble(sc.nextLine());
+					Employee e = new Employee(name, desg, sal);
+					String registration = controller.processEmployeeRegistration(e);
+					System.out.println(registration);
+					System.out.println();
+				}
+
+				case 3 -> {
+					System.out.println("Exiting........");
+					loopVar = false;
+				}
+
+				default -> System.out.println("Unexpected value: " + choice);
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println("Internal Error----" + e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println("BootIocProj05MiniProjectRealtimeDiApplication.main()-End");
+	}
+
+}
---
+++ BootIOCProj14-MiniProject-Profiles-One-YML/src/main/java/com/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootIocProj14_Working_With_Profiles_YML.class);
+	}
+
+}
